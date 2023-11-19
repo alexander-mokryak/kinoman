ee7fb39c0ea116ed0b90a493d9c713ebe8c7a477
alexander mokryak <a.mokryak87@gmail.com>
2023-11-19 13:29:02 +0300

    добаит error-view

---
+++ src/view/error-movies-view.js
@@ -0,0 +1,21 @@
+import AbstractView from '../framework/view/abstract-view';
+const createErrorMoviesViewTemplate = () => `
+    <h2 class='films-list__title'>There are no movies in our database</h2
+  >`;
+
+export default class ErrorMoviesView extends AbstractView {
+  constructor() {
+    super();
+  }
+
+  get template() {
+    return createErrorMoviesViewTemplate();
+  }
+}
+
+/* TODO Значение отображаемого текста зависит от выбранного фильтра:
+*  All movies – 'There are no movies in our database'
+*  Watchlist — 'There are no movies to watch now';
+*  History — 'There are no watched movies now';
+*  Favorites — 'There are no favorite movies now'.
+*/
